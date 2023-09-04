2fefed647c34fd5c1b9b41d2672eac35b6632f82
faysal <faysalhanif@hotmail.com>
2023-09-04 18:22:12 +0500

    Fix Badge now shows total number of items in cart

--- components/CartBadge.js
+++ components/CartBadge.js
@@ -5,24 +5,19 @@ import { StyleSheet } from 'react-native';
 import { Badge } from '@rneui/themed';
 
 const CartBadge = () => {
-    const cart = useSelector((state) => state.cart.cart);
-    console.log("Cart in badge is ", cart.length)
-    let itemCount = 0;
-    cart.length ? (itemCount = cart.length) : (itemCount = 0)
-  return (
-    <>
-        {itemCount > 0 && (
-                <Badge 
-                  value={itemCount}
-                  status="success"
-                  containerStyle={{ position: 'absolute', top: -6, right: -6 }}  
-                />
-        )
-        }
-    </>
-  );
-};
+  const cart = useSelector((state) => state.cart.cart) || [];
 
+  // Calculate the sum of the 'quantity' attribute for all items in the cart.
+  const itemCount = cart.reduce((sum, item) => sum + item.quantity, 0);
+    // cart.length ? (itemCount = cart.length) : (itemCount = 0)
+  return itemCount > 0 ? (
+      <Badge 
+        value={itemCount}
+        status="success"
+        containerStyle={{ position: 'absolute', top: -6, right: -6 }}  
+      />
+    ) : null;
+  };
 
 const styles = StyleSheet.create({});
 
