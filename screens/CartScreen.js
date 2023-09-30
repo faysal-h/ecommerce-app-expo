7a141f735f2034e0553b92868067e5228a0ddca8
faysal <faysalhanif@hotmail.com>
2023-09-30 09:28:51 +0500

    FIX Alert will be shown if cart is empty.

--- screens/CartScreen.js
+++ screens/CartScreen.js
@@ -209,7 +209,7 @@ const CartScreen = () => {
           <Text style={{ fontSize: 20, fontWeight: "bold" }}>{total?.toFixed(2)}</Text>
         </View>
         <CustomButton
-          onPress={() => createCart()}
+          onPress={cart.length? () => createCart() :Alert.alert('Empty Cart', 'Add something to place an order.')}
           customStyle={{paddingHorizontal:40, borderRadius:3, backgroundColor:'#008E97',paddingVertical:15}}
           buttonText={cart.length?`Buy ${cart.length} items`:"Cart is Empty"}
         />
